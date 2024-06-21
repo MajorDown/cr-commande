c1233c705ed09b1775c2c36b855ed18b36f43eed
MajorDown <romain.fouillaron@gmx.fr>
2024-06-21 21:54:03 +0200

    ajout de refs et nouveau build avant déploiement

--- src/data.ts
+++ src/data.ts
@@ -10,5 +10,5 @@ export const PiecesSuppliers = [
 export const PiecesPrincipalTypes = [
     "Ecran complet", "Ecran sans chassis", "lcd", "vitre tactile", "batterie", "batterie secondaire", 
     "connecteur de Charge" , "nappe lcd", "nappe cdc cm", "nappe volume", "nappe power", "nappe volume-power", 
-    "vitre arrière", "chassis", "chassis intermédiaire", "caméra avant", "caméra arrière", "verre trempé"
+    "vitre arrière", "chassis", "chassis intermédiaire", "caméra avant", "caméra arrière", "verre trempé", "lentille"
 ]
